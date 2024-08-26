13d84e192e7af4b3e32d0a0cd4968b70dad98cca
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-08-26 00:00:53 -0230

    Getting abilities to display

--- src/components/character-sheet-components/combat-options-components/CombatOptions/CombatOptions.tsx
+++ src/components/character-sheet-components/combat-options-components/CombatOptions/CombatOptions.tsx
@@ -1,54 +1,170 @@
-// import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import styles from './CombatOptions..module.css';
-// import { useCurrentID } from '../../../../hooks/useCurrentID';
-// import { getValue } from '../../../../utils/getValue';
+import { useCurrentID } from '../../../../hooks/useCurrentID';
+import { getValue } from '../../../../utils/getValue';
 // import { MoveActionList } from '../../../../data/general-actions/MoveActionList';
 // import { StandardActionList } from '../../../../data/general-actions/StandardActionList';
 // import { SwiftActionList } from '../../../../data/general-actions/SwiftActionList';
+// import { FullActionList } from '../../../../data/general-actions/FullActionList';
+// import { ReactionList } from '../../../../data/general-actions/ReactionList';
 
 function CombatOptions() {
-	// const { currentID } = useCurrentID();
+	const { currentID } = useCurrentID();
 
-	// // Create a new array of abilities to display.
-	// const [abilitiesArray, setAbilitiesArray] = useState<AbilityListTypes[]>(
-	// 	getValue(`Abilities${currentID}`)
-	// );
+	// Create a new array of abilities to display.
+	const [abilitiesArray, setAbilitiesArray] = useState<AbilityListTypes[]>(
+		getValue(`Abilities${currentID}`)
+	);
 
-	// // A list to hold all the Standard Actions.
+	// A list to hold all the Standard Actions.
 	// const [standardList, setStandardList] =
 	// 	useState<AbilityListTypes[]>(StandardActionList);
 
-	// //  A list to hold all the Move Actions.
+	//  A list to hold all the Move Actions.
 	// const [moveList, setMoveList] = useState<AbilityListTypes[]>(MoveActionList);
 
-	// // A list to hold all the Swift Actions
+	// A list to hold all the Swift Actions
 	// const [swiftList, setSwiftList] =
 	// 	useState<AbilityListTypes[]>(SwiftActionList);
 
-	// //  A list to hold all the Full Actions.
-	// const [fullList, setFullList] = useState<AbilityListTypes[]>();
+	//  A list to hold all the Full Actions.
+	// const [fullList, setFullList] = useState<AbilityListTypes[]>(FullActionList);
 
-	// const [show, setShow] = useState<boolean>(false);
+	// A list to hold all the Reactions.
+	// const [reactionList, setReaction] =
+	// 	useState<AbilityListTypes[]>(ReactionList);
 
-	// // Reset all values on character swap.
-	// useEffect(() => {
-	// 	setAbilitiesArray(getValue(`Abilities${currentID}`));
-	// 	if (show) expandCombat(show, setShow);
-	// }, [currentID]);
+	/*
+	The selected options to be shown.
+		Store the values seperately to track which actions have been taken.
+		Reaction will not be saved yet, since there's no turn order.
+		Any number of swift actions can be used, so that may be an array.
+	*/
+	const [standardAction, setStandardAction] = useState<string>('Test');
+	const [standardAvailable, setStandardAvailable] = useState<boolean>(true);
+
+	const [moveAction, setMoveAction] = useState<string>('Test');
+	const [moveAvailable, setMoveAvailable] = useState<boolean>(true);
+
+	const [fullAction, setFullAction] = useState<string>('Test');
+	const [fullAvailable, setFullAvailable] = useState<boolean>(true);
+
+	const [swiftActions, setSwiftActions] = useState<string[]>([
+		'this',
+		'is',
+		'a',
+		'test',
+	]);
+
+	const [show, setShow] = useState<boolean>(false);
+
+	// Reset all values on character swap.
+	useEffect(() => {
+		setAbilitiesArray(getValue(`Abilities${currentID}`));
+		if (show) expandCombat(show, setShow);
+	}, [currentID]);
+
+	function resetSelectedOptions() {
+		setStandardAction('');
+		setStandardAvailable(true);
+
+		setMoveAction('');
+		setMoveAvailable(true);
+
+		setFullAction('');
+		setFullAvailable(true);
+
+		setSwiftActions([]);
+	}
+
+	function syncAbilitiesList() {
+		setAbilitiesArray(getValue(`Abilities${currentID}`));
+	}
 
 	return (
 		<div className={styles.parentDiv}>
-			{/* <div className={styles.combatOptionsWrapper}>
+			<div className={styles.combatOptionsWrapper}>
 				<div
 					className={styles.combatOptionsContentWrap}
 					id={styles.combatOptionsContentWrap}
 				>
 					<div className={styles.combatOptionsLeftWrap}>
-						{standardList.map((ability) => {
+						{abilitiesArray.map((ability) => {
 							return <div>{ability.abilityName}</div>;
 						})}
+
+						<div className={styles.buttonsDiv}>
+							<div
+								className={styles.resetResyncButtons}
+								onClick={syncAbilitiesList}
+							>
+								Sync Abilities
+							</div>
+						</div>
 					</div>
-					<div className={styles.combatOptionsRightWrap}>Test</div>
+					<div className={styles.combatOptionsRightWrap}>
+						<div className={styles.selectedOptionsWrapper}>
+							<div
+								className={
+									standardAvailable
+										? styles.selectedOptionTypeAvailable
+										: styles.selectedOptionTypeUnavailable
+								}
+							>
+								Standard Action
+							</div>
+							<div className={styles.selectedOption}>{standardAction}</div>
+						</div>
+						<div className={styles.selectedOptionsWrapper}>
+							<div
+								className={
+									moveAvailable
+										? styles.selectedOptionTypeAvailable
+										: styles.selectedOptionTypeUnavailable
+								}
+							>
+								Move Action
+							</div>
+							<div className={styles.selectedOption}>{moveAction}</div>
+						</div>
+						<div className={styles.selectedOptionsWrapper}>
+							<div
+								className={
+									fullAvailable
+										? styles.selectedOptionTypeAvailable
+										: styles.selectedOptionTypeUnavailable
+								}
+							>
+								Full Action
+							</div>
+							<div className={styles.selectedOption}>{fullAction}</div>
+						</div>
+						<div className={styles.selectedOptionsWrapper}>
+							<div className={styles.selectedOptionTypeAvailable}>
+								Swift Actions
+							</div>
+							<div className={styles.selectedSwiftActions}>
+								{swiftActions.map((action, id) => {
+									return (
+										<div
+											className={styles.selectedOption}
+											key={`SwiftAction${id}`}
+										>
+											{action}
+										</div>
+									);
+								})}
+							</div>
+						</div>
+						<div className={styles.buttonsDiv}>
+							<div
+								className={styles.resetResyncButtons}
+								onClick={resetSelectedOptions}
+							>
+								Reset Round
+							</div>
+						</div>
+					</div>
 				</div>
 				<div className={styles.showCombatOptions}>
 					<div
@@ -59,29 +175,29 @@ function CombatOptions() {
 						{show ? 'End' : 'Start'} Combat
 					</div>
 				</div>
-			</div> */}
+			</div>
 		</div>
 	);
 }
 
 // Function to show/hide the component.
-// function expandCombat(
-// 	show: boolean,
-// 	setShow: Dispatch<SetStateAction<boolean>>
-// ) {
-// 	setShow(!show);
+function expandCombat(
+	show: boolean,
+	setShow: Dispatch<SetStateAction<boolean>>
+) {
+	setShow(!show);
 
-// 	const toggleButton: HTMLElement = document.getElementById(
-// 		styles.showCombatOptionsButton
-// 	) as HTMLDivElement;
+	const toggleButton: HTMLElement = document.getElementById(
+		styles.showCombatOptionsButton
+	) as HTMLDivElement;
 
-// 	const toggleOptions: HTMLElement = document.getElementById(
-// 		styles.combatOptionsContentWrap
-// 	) as HTMLDivElement;
+	const toggleOptions: HTMLElement = document.getElementById(
+		styles.combatOptionsContentWrap
+	) as HTMLDivElement;
 
-// 	toggleButton.classList.toggle(styles.showCombatOptionsButtonShow);
+	toggleButton.classList.toggle(styles.showCombatOptionsButtonShow);
 
-// 	toggleOptions.classList.toggle(styles.show);
-// }
+	toggleOptions.classList.toggle(styles.show);
+}
 
 export default CombatOptions;
