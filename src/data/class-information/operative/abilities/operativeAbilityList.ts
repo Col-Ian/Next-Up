2c43c27fe343db3b0063f81be861041dafdb4750
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-08-27 00:28:08 -0230

    Fixed type issue in operativeAbilityList

--- src/data/class-information/operative/abilities/operativeAbilityList.ts
+++ src/data/class-information/operative/abilities/operativeAbilityList.ts
@@ -22,7 +22,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'Your diverse training as an operative grants you a +1 insight bonus to initiative checks and to skill checks. This bonus increases by 1 at 3rd level and every 4 levels thereafter.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 			SPECIALIZATION: {
@@ -30,7 +30,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					"Your specialization represents your primary area of expertise. Pick one specialisation upon taking your 1st level of the operative class. Once made, this choice cannot be changed. Descriptions of the specialiations appear on page 94 of the Core Rulebook. Your specialization grants you the Skill Focus feat (see page 161 of the Core Rulebook) in your specialization's associated skills, and you gain a free skill rank in each of those skills at each operative level (this does not allow you to exceed the maximum number of skill ranks in a single skill).",
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 			'TRICK ATTACK [EX]': {
@@ -38,7 +38,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					"You can trick or startle a foe and then attack when they drop their guard. As a full action, you can move up to your speed. Whether or not you moved, you can then make an attack with a melee weapon with the operative special property or with any small arm. Just before making your attack, attempt a Bluff, Intimidate, or Stealth check (or a check associated with your specialization; see page 94 of the Core Rulebook) with a DC equal to 20 + your target's CR. If you succeed at the check, you deal 1d4 additional damage and the target is flat-footed against your attack. This damage increases to 1d8 at 3rd level, to 3d8 at 5th level, and by an additional 1d8 every 2 levels thereafter. You can't use this ability with a weapon that has the unwieldy special property or that requires a full action to make a single attack.",
 				abilitySource: 'Operative',
-				actionType: ['Full Action'],
+				actionType: [false, false, false, true, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -58,7 +58,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'If you succeed at a Reflex save against an effect that normally has a partial effect on a successful save, you instead suffer no effect. You gain this benefit only when unencumbered and wearing light armor or no armor, and you lose the benefit when you are helpless or otherwise unable to move.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -81,7 +81,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'As long as you are unencumbered and wearing light armor or no armor, your land speed increases by 10 feet. At 9th level, your land speed instead increases by 20 feet, and at 15th level, your land speed instead increases by 30 feet.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 			'Weapon Specialization (Ex)': {
@@ -89,7 +89,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'You gain the Weapon Specialization feat as a bonus feat for each weapon type with which this class grants you proficiency.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -109,7 +109,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'When you succeed at both the skill check and attack roll of your trick attack, you can make the target flat-footed or off-target until the beginning of your next turn. You might learn exploits that grant you additional options for your debilitating trick, but you can select only one option each time you hit with a trick attack.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -157,7 +157,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'You become so confident in certain skills that you can use them reliably even under adverse conditions. When attempting a skill check with a skill in which you have the Skill Focus feat, you can take 10 even if stress or distractions would normally prevent you from doing so.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 			'Uncanny Agility': {
@@ -165,7 +165,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					"You are immune to the flat-footed condition, and your opponent doesn't gain any bonuses to attack rolls against you from flanking you or attacking you when you're prone. Furthermore, covering fire and harrying fire don't provide any advantage against you.",
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -185,7 +185,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'When making a full attack using only melee weapons with the operative special property or small arms, you can make up to three attacks instead of two.',
 				abilitySource: 'Operative',
-				actionType: ['Full Action'],
+				actionType: [false, false, false, true, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -252,7 +252,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'When making a full attack using only melee weapons with the operative special property or small arms, you can make up to four attacks instead of two.',
 				abilitySource: 'Operative',
-				actionType: ['Full Action'],
+				actionType: [false, false, false, true, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -312,7 +312,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					'When you succeed at both the skill check and attack roll of your trick attack, you can apply two of your debilitating trick effects.',
 				abilitySource: 'Operative',
-				actionType: ['None'],
+				actionType: [false, false, false, false, false, false],
 				usesResolve: 0,
 			},
 		},
@@ -357,7 +357,7 @@ export const operativeAbilityList: {
 				abilityDescription:
 					"Whenever you attempt a skill check with your specialization's associated skills, you can roll twice and take the higher result. Once per day as a move action, you can temporarily trade out one of your operative exploits for another operative exploit requiring the same level or lower. This trade lasts 24 hours. If the exploit you trade away is a prerequisite for any of your other abilities, you lose all abilities that require it as a prerequisite for the duration.",
 				abilitySource: 'Operative',
-				actionType: ['Move Action'],
+				actionType: [false, true, false, false, false, false],
 				usesResolve: 0,
 			},
 		},
