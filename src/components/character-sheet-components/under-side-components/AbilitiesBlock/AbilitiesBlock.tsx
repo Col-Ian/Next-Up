576d47bd7a3364de6759c134be76950f1e6a5804
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-09-07 23:20:17 -0230

    Added Expand button with sticky to abilities list.

--- src/components/character-sheet-components/under-side-components/AbilitiesBlock/AbilitiesBlock.tsx
+++ src/components/character-sheet-components/under-side-components/AbilitiesBlock/AbilitiesBlock.tsx
@@ -1,9 +1,10 @@
 import SheetLabel from '../../labels/BlueLabel/SheetLabel';
 import styles from './AbilitiesBlock.module.css';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import AddButtonLabel from '../../AddButtonLabel/AddButtonLabel';
 import { FieldValues, useFieldArray, useForm } from 'react-hook-form';
 import { CharacterSheetContext } from '../../../../states/CharacterSheet/CharacterSheet';
+import ExpandComponent from '../../ExpandComponent/ExpandComponent';
 
 type FormValues = FieldValues & {
 	name: AbilityListTypes[];
@@ -15,6 +16,8 @@ function AbilitiesBlock() {
 	);
 
 	const { control, register, watch, reset } = useForm<FormValues>();
+
+	const [showAbilities, setShowAbilities] = useState<boolean>(false);
 
 	const { fields, append, remove } = useFieldArray<
 		FormValues,
@@ -60,119 +63,129 @@ function AbilitiesBlock() {
 		<div className={styles.parentDiv}>
 			<div className={styles.labelDiv}>
 				<SheetLabel sheetLabelText='ABILITIES' />
-				<div
-					className={styles.addAbilityButton}
-					onClick={() => {
-						append({
-							abilityName: '',
-							abilityDescription: '',
-							abilitySource: '',
-							actionType: [false, false, false, false, false, false],
-							usesResolve: 0,
-						});
-					}}
-				>
-					<AddButtonLabel itemToAdd='ABILITY' />
-				</div>
+				{showAbilities ? (
+					<div
+						className={styles.addAbilityButton}
+						onClick={() => {
+							append({
+								abilityName: '',
+								abilityDescription: '',
+								abilitySource: '',
+								actionType: [false, false, false, false, false, false],
+								usesResolve: 0,
+							});
+						}}
+					>
+						<AddButtonLabel itemToAdd='ABILITY' />
+					</div>
+				) : null}
 			</div>
 			<div className={styles.abilitiesBlockContent}>
-				{fields.map((field, index) => {
-					return (
-						<div className={styles.individualAbility} key={field.id}>
-							<div className={styles.leftBlock}>
-								<div
-									className={styles.delete}
-									onClick={() => handleRemove(index)}
-								>
-									&#128465;
-								</div>
-								<div className={styles.topRow}>
-									<div className={styles.inputDiv}>
-										<input
-											type='text'
-											{...register(`abilities.${index}.abilityName`)}
-											spellCheck={false}
-											className={styles.textInput}
-										/>
+				{showAbilities
+					? fields.map((field, index) => {
+							return (
+								<div className={styles.individualAbility} key={field.id}>
+									<div className={styles.leftBlock}>
+										<div
+											className={styles.delete}
+											onClick={() => handleRemove(index)}
+										>
+											&#128465;
+										</div>
+										<div className={styles.topRow}>
+											<div className={styles.inputDiv}>
+												<input
+													type='text'
+													{...register(`abilities.${index}.abilityName`)}
+													spellCheck={false}
+													className={styles.textInput}
+												/>
+											</div>
+											<div className={styles.verticalBar} />
+											<div className={styles.inputDiv}>
+												<input
+													type='text'
+													{...register(`abilities.${index}.abilitySource`)}
+													spellCheck={false}
+													className={styles.textInput}
+												/>
+											</div>
+											<div className={styles.verticalBar} />
+											<div className={styles.inputDiv}>
+												<div className={styles.inputLabel}>RP</div>
+												<input
+													type='number'
+													{...register(`abilities.${index}.usesResolve`)}
+													className={styles.numberInput}
+												/>
+											</div>
+										</div>
+										<div className={styles.abilityDescription}>
+											<textarea
+												{...register(`abilities.${index}.abilityDescription`)}
+												className={styles.abilityTextarea}
+												spellCheck={false}
+											/>
+										</div>
 									</div>
-									<div className={styles.verticalBar} />
-									<div className={styles.inputDiv}>
-										<input
-											type='text'
-											{...register(`abilities.${index}.abilitySource`)}
-											spellCheck={false}
-											className={styles.textInput}
-										/>
-									</div>
-									<div className={styles.verticalBar} />
-									<div className={styles.inputDiv}>
-										<div className={styles.inputLabel}>RP</div>
-										<input
-											type='number'
-											{...register(`abilities.${index}.usesResolve`)}
-											className={styles.numberInput}
-										/>
-									</div>
-								</div>
-								<div className={styles.abilityDescription}>
-									<textarea
-										{...register(`abilities.${index}.abilityDescription`)}
-										className={styles.abilityTextarea}
-										spellCheck={false}
-									/>
-								</div>
-							</div>
 
-							<div className={styles.typeDiv}>
-								<div className={styles.typeDivLabel}>Action</div>
-								<div className={styles.actionTypeDropDown}>
-									<div className={styles.actionRow}>
-										<input
-											type='checkbox'
-											{...register(`abilities.${index}.actionType.0`)}
-										/>
-										<div className={styles.actionLabel}>Standard</div>
-									</div>
-									<div className={styles.actionRow}>
-										<input
-											type='checkbox'
-											{...register(`abilities.${index}.actionType.1`)}
-										/>
-										<div className={styles.actionLabel}>Move</div>
-									</div>
-									<div className={styles.actionRow}>
-										<input
-											type='checkbox'
-											{...register(`abilities.${index}.actionType.2`)}
-										/>
-										<div className={styles.actionLabel}>Swift</div>
-									</div>
-									<div className={styles.actionRow}>
-										<input
-											type='checkbox'
-											{...register(`abilities.${index}.actionType.3`)}
-										/>
-										<div className={styles.actionLabel}>Full</div>
-									</div>
-									<div className={styles.actionRow}>
-										<input
-											type='checkbox'
-											{...register(`abilities.${index}.actionType.4`)}
-										/>
-										<div className={styles.actionLabel}>Reaction</div>
-									</div>
-									<div className={styles.actionRow}>
-										<input
-											type='checkbox'
-											{...register(`abilities.${index}.actionType.5`)}
-										/>
-										<div className={styles.actionLabel}>Other</div>
+									<div className={styles.typeDiv}>
+										<div className={styles.typeDivLabel}>Action</div>
+										<div className={styles.actionTypeDropDown}>
+											<div className={styles.actionRow}>
+												<input
+													type='checkbox'
+													{...register(`abilities.${index}.actionType.0`)}
+												/>
+												<div className={styles.actionLabel}>Standard</div>
+											</div>
+											<div className={styles.actionRow}>
+												<input
+													type='checkbox'
+													{...register(`abilities.${index}.actionType.1`)}
+												/>
+												<div className={styles.actionLabel}>Move</div>
+											</div>
+											<div className={styles.actionRow}>
+												<input
+													type='checkbox'
+													{...register(`abilities.${index}.actionType.2`)}
+												/>
+												<div className={styles.actionLabel}>Swift</div>
+											</div>
+											<div className={styles.actionRow}>
+												<input
+													type='checkbox'
+													{...register(`abilities.${index}.actionType.3`)}
+												/>
+												<div className={styles.actionLabel}>Full</div>
+											</div>
+											<div className={styles.actionRow}>
+												<input
+													type='checkbox'
+													{...register(`abilities.${index}.actionType.4`)}
+												/>
+												<div className={styles.actionLabel}>Reaction</div>
+											</div>
+											<div className={styles.actionRow}>
+												<input
+													type='checkbox'
+													{...register(`abilities.${index}.actionType.5`)}
+												/>
+												<div className={styles.actionLabel}>Other</div>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-					);
-				})}
+							);
+					  })
+					: null}
+				<div
+					className={styles.dropDownWrapper}
+					onClick={() => setShowAbilities(!showAbilities)}
+				>
+					<ExpandComponent expanded={showAbilities} />
+				</div>
 			</div>
 		</div>
 	);
