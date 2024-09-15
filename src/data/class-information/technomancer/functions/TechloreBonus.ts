525a128800e8ad0ff40d7d77ee0553d0281ae8d3
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-09-14 23:29:15 -0230

    Fixed function value error.

--- src/data/class-information/technomancer/functions/TechloreBonus.ts
+++ src/data/class-information/technomancer/functions/TechloreBonus.ts
@@ -2,19 +2,19 @@ import { getValue } from '../../../../utils/getValue';
 import { setValue } from '../../../../utils/setValue';
 
 export default function TechloreBonus(characterID: string) {
-	const characterLevel: number = getValue(`Level${characterID}`);
+	const characterLevel: number = getValue(`Level${characterID}`) + 1;
 
 	let computersSkill: SkillListType = getValue(`Computers${characterID}`);
 
 	let mysticismSkill: SkillListType = getValue(`Mysticism${characterID}`);
 
-	if (computersSkill.insightBonusToRank < characterLevel / 3) {
-		computersSkill.insightBonusToRank = characterLevel / 3;
+	if (computersSkill.insightBonusToValue < characterLevel / 3) {
+		computersSkill.insightBonusToValue = characterLevel / 3;
 		setValue(`Computers${characterID}`, computersSkill);
 	}
 
-	if (mysticismSkill.insightBonusToRank < characterLevel / 3) {
-		mysticismSkill.insightBonusToRank = characterLevel / 3;
+	if (mysticismSkill.insightBonusToValue < characterLevel / 3) {
+		mysticismSkill.insightBonusToValue = characterLevel / 3;
 		setValue(`Mysticism${characterID}`, mysticismSkill);
 	}
 }
